1d211c066e8cc400fc5548c3ea5f275d3b18efa5
Madi Knox <madeleinelknox@gmail.com>
2020-07-10 16:21:35 -0400

    chore:deleted room table from db

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,7 +2,6 @@ const User = require('./user')
 const Game = require('./game')
 const Player = require('./player')
 const WordPrompts = require('./wordPrompts')
-const Room = require('./room')
 
 User.belongsToMany(Game, {through: Player})
 Game.belongsToMany(User, {through: Player})
@@ -10,13 +9,9 @@ Game.belongsToMany(User, {through: Player})
 Game.belongsToMany(WordPrompts, {through: 'gamePrompts'})
 WordPrompts.belongsToMany(Game, {through: 'gamePrompts'})
 
-// Room.hasOne(Game)
-// Game.belongsTo(Room)
-
 module.exports = {
   User,
   Game,
   Player,
-  WordPrompts,
-  Room
+  WordPrompts
 }
