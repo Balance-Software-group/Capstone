cf11d79639748cef1ab4b8b5fd0cafa79e133f24
FeliciaKh <feliciakh0428@gmail.com>
2020-07-11 13:32:18 -0400

    Add text box for chat

--- client/components/ChatBox.js
+++ client/components/ChatBox.js
@@ -4,7 +4,20 @@ import {Button, Comment, Form, Header} from 'semantic-ui-react'
 export const ChatBox = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Comment.Group>
+        <Header as="h3" dividing>
+          Type your guesses here and chat with your friends!
+        </Header>
+        <Form reply>
+          <Form.TextArea />
+          <Button
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+            primary
+          />
+        </Form>
+      </Comment.Group>
     </div>
   )
 }
