f6cde049cdcedcbf87eb3fc396c20134b88771bb
FeliciaKh <feliciakh0428@gmail.com>
2020-07-11 16:57:24 -0400

    Show socket room and user connection

--- client/components/ChatBox.js
+++ client/components/ChatBox.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import queryString from 'querystring'
+import io from 'socket.io-client'
 import {Button, Comment, Form, Header} from 'semantic-ui-react'
 
-export const ChatBox = () => {
+let socket
+
+export const ChatBox = ({location}) => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  const ENDPOINT = 'localhost:8080'
+  //UPDATE: when we make the rooms live
+
+  useEffect(
+    () => {
+      const {name, room} = queryString.parse(location.search)
+
+      socket = io(ENDPOINT)
+
+      setName(name)
+      setRoom(room)
+
+      socket.emit('join', {name, room})
+
+      return () => {
+        socket.emit('disconnect')
+        socket.off()
+      }
+    },
+    [ENDPOINT, location.search]
+  )
   return (
     <div>
       <Comment.Group>
--- server/socket/index.js
+++ server/socket/index.js
@@ -2,6 +2,12 @@ module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
+    socket.on('join', ({name, room}, callback) => {
+      console.log(name, room)
+
+      callback()
+    })
+
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
