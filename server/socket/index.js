e60f7a76e6b360d1063e8faae0bdc7d503f7d1f5
FeliciaKh <feliciakh0428@gmail.com>
2020-07-12 10:57:55 -0400

    Backend connections for Add and Get user for socket

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,9 +1,29 @@
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users')
 module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
     socket.on('join', ({name, room}, callback) => {
-      console.log(name, room)
+      const {error, user} = addUser({id: socket.id, name, room})
+      if (error) return callback(error)
+
+      socket.emit('message', {
+        user: 'admin',
+        text: `${user.name}, welcome to the ${user.room} room!`
+      })
+      socket.broadcast
+        .to(user.room)
+        .emit('message', {user: 'admin', text: `${user.name} has joined!`})
+
+      socket.join(user.room)
+
+      callback()
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+      const user = getUser(socket.id)
+
+      io.to(user.room).emit('message', {user: user.name, text: message})
 
       callback()
     })
